eb8e60790f0af6b9d513abb985a26b2a6fd7f588
Dang Hieu <phungdanghieu1842003@gmail.com>
2024-01-30 02:56:30 +0700

    update-achievement

--- pyGame/game.py
+++ pyGame/game.py
@@ -9,6 +9,36 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
+high_score = 0
+
+# Show achievement
+def show_achievements_screen(dis, dis_width, dis_height):
+    achievements_screen = True
+
+    while achievements_screen:
+        dis.fill(BLACK)
+        font = pygame.font.SysFont(None, 30)
+        text = font.render("High Score: " + str(high_score), True, (255, 255, 255))
+        dis.blit(text, (dis_width / 2 - 100, dis_height / 2 - 50))
+        
+        pygame.draw.rect(dis, (255, 255, 255), [dis_width / 2 - 70, dis_height / 2 + 50, 140, 40])
+        button_font = pygame.font.SysFont(None, 30)
+        button_text = button_font.render("Close", True, (0, 0, 0))
+        button_rect = button_text.get_rect(center=(dis_width / 2, dis_height / 2 + 70))
+        dis.blit(button_text, button_rect)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseX, mouseY = pygame.mouse.get_pos()
+                close_button_rect = pygame.Rect(dis_width / 2 - 70, dis_height / 2 + 50, 140, 40)
+                if close_button_rect.collidepoint(mouseX, mouseY):
+                    dis.fill(BLACK)
+                    achievements_screen = False
 
 # Show message
 def message(dis, msg, color, dis_width, dis_height):
@@ -19,7 +49,7 @@ def message(dis, msg, color, dis_width, dis_height):
     dis.blit(mesg, text_rect)
     pygame.display.update()
 
-# Display Play button
+# Play button
 def display_play_button(dis_width, dis_height, dis):
     pygame.draw.rect(dis, GREEN, [dis_width / 2 - 70, dis_height / 2 + 20, 140, 40])
     button_font = pygame.font.SysFont(None, 30)
@@ -28,7 +58,18 @@ def display_play_button(dis_width, dis_height, dis):
     dis.blit(button_text, button_rect)
 
     pygame.display.update()
+
+# Achievement button
+def display_achievements_button(dis_width, dis_height, dis):
+    pygame.draw.rect(dis, (255, 165, 0), [dis_width / 2 - 70, dis_height / 2 + 120, 140, 40])
+    button_font = pygame.font.SysFont(None, 30)
+    button_text = button_font.render("Achievements", True, (0, 0, 0))
+    button_rect = button_text.get_rect(center=(dis_width / 2, dis_height / 2 + 140))
+    dis.blit(button_text, button_rect)
     
+    pygame.display.update()
+    
+# Continue button 
 def display_continues_button(dis_width, dis_height, dis):
     pygame.draw.rect(dis, GREEN, [dis_width / 2 - 70, dis_height / 2 + 20, 140, 40])
     button_font = pygame.font.SysFont(None, 30)
@@ -38,7 +79,7 @@ def display_continues_button(dis_width, dis_height, dis):
 
     pygame.display.update()
 
-# Display Quit button
+# Quit button
 def display_quit_button(dis_width, dis_height, dis):
     pygame.draw.rect(dis, RED, [dis_width / 2 - 70, dis_height / 2 + 70, 140, 40])
     button_font = pygame.font.SysFont(None, 30)
@@ -46,7 +87,7 @@ def display_quit_button(dis_width, dis_height, dis):
     button_rect = button_text.get_rect(center=(dis_width / 2, dis_height / 2 + 90))
     dis.blit(button_text, button_rect)
 
-# Display Replay button
+# Replay button
 def display_replay_button(dis_width, dis_height, dis):
     pygame.draw.rect(dis, YELLOW, [dis_width / 2 - 70, dis_height / 2 + 20, 140, 40])
     button_font = pygame.font.SysFont(None, 30)
@@ -58,10 +99,12 @@ def display_replay_button(dis_width, dis_height, dis):
 # Logic game
 def gameLoop(dis_width, dis_height, dis, pygame, clock, snake_block, initial_snake_speed):
     play_button_clicked = False
+    achievements_button_clicked = False
     quit_button_clicked = False
     replay_button_clicked = False
     paused = False
     snake_speed = initial_snake_speed
+    return_to_main = False
     
     while not play_button_clicked:
         for event in pygame.event.get():
@@ -72,12 +115,16 @@ def gameLoop(dis_width, dis_height, dis, pygame, clock, snake_block, initial_sna
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouseX, mouseY = pygame.mouse.get_pos()
                 play_button_rect = pygame.Rect(dis_width / 2 - 70, dis_height / 2 + 20, 140, 40)
-
+                achievements_button_rect = pygame.Rect(dis_width / 2 - 70, dis_height / 2 + 120, 140, 40)
+                
                 if play_button_rect.collidepoint(mouseX, mouseY):
                     play_button_clicked = True
+                elif achievements_button_rect.collidepoint(mouseX, mouseY):
+                    show_achievements_screen(dis, dis_width, dis_height) 
 
         message(dis, "Snake Game", WHITE, dis_width, dis_height)
         display_play_button(dis_width, dis_height, dis)
+        display_achievements_button(dis_width, dis_height, dis)
         pygame.display.update()
 
     game_over = False
@@ -97,11 +144,16 @@ def gameLoop(dis_width, dis_height, dis, pygame, clock, snake_block, initial_sna
 
     while not game_over and not replay_button_clicked or not quit_button_clicked:
         while game_close == True:
+            if return_to_main:
+                break
             Your_score(dis, length_of_snake - 1)
             message(dis, "Game Over", (213, 50, 80), dis_width, dis_height)
             display_replay_button(dis_width, dis_height, dis)
             display_quit_button(dis_width, dis_height, dis)
             pygame.display.update()
+            
+            if length_of_snake - 1 > high_score:
+                high_score = length_of_snake - 1
 
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
@@ -124,6 +176,7 @@ def gameLoop(dis_width, dis_height, dis, pygame, clock, snake_block, initial_sna
                     if quit_button_rect.collidepoint(mouseX, mouseY):
                         quit_button_clicked = True
                         game_over = True
+                        return_to_main = True
                         game_close = False
 
         for event in pygame.event.get():
@@ -161,7 +214,8 @@ def gameLoop(dis_width, dis_height, dis, pygame, clock, snake_block, initial_sna
                         paused = False
                     elif quit_button_rect.collidepoint(mouseX, mouseY):
                         quit_button_clicked = True
-                        game_over = True
+                        return_to_main = True
+                        game_close = False
                         paused = False
 
         if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
